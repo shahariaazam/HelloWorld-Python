b73950825a106814149cb737fc1b65d1152dcc74
Shaharia Azam <shaharia.azam@gmail.com>
2017-02-24 17:53:14 +0600

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+# Print a variable
+helloWorldString = "This is a hello world string"
+print helloWorldString
+
+# Arithmetic Operation
+firstNumber = 100.2
+secondNumber = 20.34
+print firstNumber + secondNumber
+print firstNumber - secondNumber
+print firstNumber / secondNumber
+print firstNumber * secondNumber
+
+
+#Control Structure
+if firstNumber > secondNumber:
+    print "first number is greater than second number"
+else:
+    print "second number is greater than first number"
+
+
+#Loops
+value = 0
+
+for letter in "Shaharia Azam":
+    if letter == "h":
+        value = value + 1
+        print "I love this word"
+
+print "total h", value
